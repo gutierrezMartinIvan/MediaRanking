0b76c984d93703672e2ddd6ef85484794cb20f24
ignaciomoyanosm <ignaciomoyanosm@hotmail.com>
2022-12-29 12:19:14 -0300

    Add Series

---
+++ src/main/java/com/media/ranking/ar/models/entity/Series.java
@@ -0,0 +1,13 @@
+package com.media.ranking.ar.models.entity;
+
+import java.util.List;
+
+public class Series {
+    long id;
+    String title;
+    String description;
+    String author;
+    List<String> genres;
+    String dateRelease;
+}
+
