006646d9a40f54a5d22c9b0921bbc22615852078
Martin Pata <mpata2000@gmail.com>
2022-12-29 12:07:22 -0300

    Add movie model

---
+++ src/main/java/com/media/ranking/ar/models/entity/Movie.java
@@ -0,0 +1,12 @@
+package com.media.ranking.ar.models.entity;
+
+import java.util.List;
+
+public class Movie {
+    long id;
+    String title;
+    String description;
+    String director;
+    List<String> genres;
+    int  duration;
+}
