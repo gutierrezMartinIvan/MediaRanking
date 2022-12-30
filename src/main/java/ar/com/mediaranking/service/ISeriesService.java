bd79e563778eb0a09f4bb51c34d0734bd6c99eae
beniko19 <martin192012@gmail.com>
2022-12-30 10:48:35 -0300

    added the empty service for Series

---
+++ src/main/java/ar/com/mediaranking/service/ISeriesService.java
@@ -0,0 +1,4 @@
+package ar.com.mediaranking.service;
+
+public interface ISeriesService {
+}
---
+++ src/main/java/ar/com/mediaranking/service/impl/SeriesServiceImpl.java
@@ -0,0 +1,8 @@
+package ar.com.mediaranking.service.impl;
+
+import ar.com.mediaranking.service.ISeriesService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SeriesServiceImpl implements ISeriesService {
+}
