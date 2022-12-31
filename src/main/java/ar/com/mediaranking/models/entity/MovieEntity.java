1e52c67d0e5567f9e8bf7c1e15361861157d6dd7
Martin Pata <mpata2000@gmail.com>
2022-12-31 12:02:51 -0300

    Added docker db

--- src/main/java/ar/com/mediaranking/models/entity/MovieEntity.java
+++ src/main/java/ar/com/mediaranking/models/entity/MovieEntity.java
@@ -1,13 +1,19 @@
 package ar.com.mediaranking.models.entity;
 
+import jakarta.persistence.*;
+
 import java.util.List;
 
+@Entity
+@Table(name = "series")
 public class MovieEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
-    String title;
+    /*String title;
     String description;
     String director;
     List<String> genres;
     int  duration;
-    ReviewEntity review;
+    ReviewEntity review;*/
 }
--- src/main/java/ar/com/mediaranking/models/entity/SeriesEntity.java
+++ src/main/java/ar/com/mediaranking/models/entity/SeriesEntity.java
@@ -1,15 +1,22 @@
 package ar.com.mediaranking.models.entity;
 
+import jakarta.persistence.*;
+
 import java.util.List;
 
+@Entity
+@Table(name = "series")
 public class SeriesEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+    /*
     String title;
     String description;
     String author;
     List<String> genres;
     String dateRelease;
     List<SeasonEntity> seasons;
-    ReviewEntity review;
+    ReviewEntity review;*/
 }
 
