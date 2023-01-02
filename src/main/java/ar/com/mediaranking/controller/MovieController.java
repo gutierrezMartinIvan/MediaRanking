a501cd5c6c8d58b792949feb65218b8aeb2939f5
Martin Pata <mpata2000@gmail.com>
2023-01-02 15:50:00 -0300

    Added put and delete to controller

--- src/main/java/ar/com/mediaranking/controller/MovieController.java
+++ src/main/java/ar/com/mediaranking/controller/MovieController.java
@@ -16,17 +16,29 @@ public class MovieController {
     private MovieRepository movieRepository;
 
     @GetMapping
-    public List<MovieEntity> getAllMovies() {
+    public List<MovieEntity> getMovies(@RequestParam(required = false) String title,
+                                          @RequestParam(required = false) String director,
+                                          @RequestParam(required = false) String genre) {
         return movieRepository.findAll();
     }
 
-    /*@GetMapping
-    public MovieEntity getMovieById(@RequestParam long id, @RequestParam String title, @RequestParam String description, @RequestParam String director, @RequestParam List<String> genres, @RequestParam int duration, @RequestParam ReviewEntity review) {
-        return movieRepository.findById(1L).orElse(null);
-    }*/
+    @GetMapping("/{id}")
+    public MovieEntity getMovieById(@PathVariable Long id) {
+        return movieRepository.findById(id).orElse(null);
+    }
 
     @PostMapping
     public MovieEntity createMovie(@RequestBody MovieEntity movie) {
+        return movieRepository.save(movie);
+    }
+
+    @DeleteMapping
+    public void deleteMovie(@RequestParam long id) {
+        movieRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public MovieEntity updateMovie(@RequestBody MovieEntity movie) {
         return movieRepository.save(movie);
     }
 }
