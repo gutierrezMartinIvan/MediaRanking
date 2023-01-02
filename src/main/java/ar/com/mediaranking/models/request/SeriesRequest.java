91176a956ad65f9de2d6b81222081e39e3b9e597
beniko19 <martin192012@gmail.com>
2023-01-02 15:59:50 -0300

    added the year attribute to the entity and added the request and response

--- src/main/java/ar/com/mediaranking/models/request/SeriesRequest.java
+++ src/main/java/ar/com/mediaranking/models/request/SeriesRequest.java
@@ -1,47 +1,31 @@
-package ar.com.mediaranking.models.entity;
+package ar.com.mediaranking.models.request;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.*;
+import lombok.Data;
 
-import java.util.List;
-
-@Entity
-@Table(name = "series")
-public class SeriesEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "series_id")
-    Long id;
-
-    @Column(nullable = false)
+@Data
+public class SeriesRequest {
     @NotNull(message = "Tittle can not be null")
     @NotBlank(message = "Tittle can not be blank")
     @NotEmpty(message = "Tittle can not be empty")
     @Pattern(regexp =  "^[A-Za-z0-9!@#$%^&*()_+=-]*$", message = "Tittle only can contain letters, numbers and special characters")
     String title;
-    @Column(nullable = false)
+
     @NotNull(message = "Description can not be null")
     @NotBlank(message = "Description can not be blank")
     @NotEmpty(message = "Description can not be empty")
     String description;
-    @Column(nullable = false)
+
     @NotNull(message = "Description can not be null")
     @NotBlank(message = "Description can not be blank")
     @NotEmpty(message = "Description can not be empty")
     String author;
 
-    @Column(nullable = false)
     @Min(1800)
     @Max(2023)
     @NotNull(message = "Description can not be null")
     @NotBlank(message = "Description can not be blank")
     @NotEmpty(message = "Description can not be empty")
     Short year;
-
-    /*
-    List<String> genres;
-    String dateRelease;
-    List<SeasonEntity> seasons;
-    ReviewEntity review;*/
 }
-
--- src/main/java/ar/com/mediaranking/models/response/SeriesResponse.java
+++ src/main/java/ar/com/mediaranking/models/response/SeriesResponse.java
@@ -1,47 +1,30 @@
-package ar.com.mediaranking.models.entity;
+package ar.com.mediaranking.models.response;
 
-import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 
-import java.util.List;
+public class SeriesResponse {
 
-@Entity
-@Table(name = "series")
-public class SeriesEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "series_id")
     Long id;
-
-    @Column(nullable = false)
     @NotNull(message = "Tittle can not be null")
     @NotBlank(message = "Tittle can not be blank")
     @NotEmpty(message = "Tittle can not be empty")
     @Pattern(regexp =  "^[A-Za-z0-9!@#$%^&*()_+=-]*$", message = "Tittle only can contain letters, numbers and special characters")
     String title;
-    @Column(nullable = false)
+
     @NotNull(message = "Description can not be null")
     @NotBlank(message = "Description can not be blank")
     @NotEmpty(message = "Description can not be empty")
     String description;
-    @Column(nullable = false)
+
     @NotNull(message = "Description can not be null")
     @NotBlank(message = "Description can not be blank")
     @NotEmpty(message = "Description can not be empty")
     String author;
 
-    @Column(nullable = false)
     @Min(1800)
     @Max(2023)
     @NotNull(message = "Description can not be null")
     @NotBlank(message = "Description can not be blank")
     @NotEmpty(message = "Description can not be empty")
     Short year;
-
-    /*
-    List<String> genres;
-    String dateRelease;
-    List<SeasonEntity> seasons;
-    ReviewEntity review;*/
 }
-
