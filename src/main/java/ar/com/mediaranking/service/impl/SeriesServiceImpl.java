a26de8428f651f6f24dc8433f0316deef6391e24
beniko19 <martin192012@gmail.com>
2023-01-02 16:35:37 -0300

    created the implementations for the series service

--- src/main/java/ar/com/mediaranking/service/impl/SeriesServiceImpl.java
+++ src/main/java/ar/com/mediaranking/service/impl/SeriesServiceImpl.java
@@ -1,8 +1,41 @@
 package ar.com.mediaranking.service.impl;
 
+import ar.com.mediaranking.models.entity.SeriesEntity;
+import ar.com.mediaranking.models.repository.ISeriesRepository;
+import ar.com.mediaranking.models.request.SeriesRequest;
+import ar.com.mediaranking.models.response.SeriesResponse;
 import ar.com.mediaranking.service.ISeriesService;
+import ar.com.mediaranking.utils.DtoToEntityConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SeriesServiceImpl implements ISeriesService {
+
+    @Autowired
+    private ISeriesRepository repository;
+
+    @Autowired
+    private DtoToEntityConverter mapper;
+
+    @Override
+    public boolean isNull(SeriesRequest request) {
+        return false;
+    }
+
+    @Override
+    public SeriesResponse save(SeriesRequest request) /*throws NameOrContentAreNull*/ {
+        SeriesEntity entitySave = mapper.convertDtoToEntity(request);
+        SeriesResponse response = mapper.convertEntityToDto(repository.save(entitySave));
+        return response;
+    }
+
+    @Override
+    public List<SeriesResponse> getAll() {
+        return mapper.convertEntityToDto(repository.findAll());
+    }
+
+
 }
