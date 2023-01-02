77511d412f18caf6981cbd83c55f3bcfc15622f6
Martin Pata <mpata2000@gmail.com>
2023-01-02 15:34:43 -0300

    Added OpenAPI

---
+++ src/main/java/ar/com/mediaranking/config/OpenAPIConfig.java
@@ -0,0 +1,34 @@
+package ar.com.mediaranking.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+
+public class OpenAPIConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("movies")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Eldar Academy Springboot API")
+                .version("1.0")
+                .contact(apiContact());
+    }
+
+    private Contact apiContact() {
+        return new Contact().name("Martin Pata").email("martin.pata@eldars.com.ar").url("https://github.com/mpata2000");
+    }
+}
