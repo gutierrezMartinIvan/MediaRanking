6cee9c2064b0649d1218d86569c9f18a9f3dc2e8
beniko19 <martin192012@gmail.com>
2023-01-02 16:36:18 -0300

    added the save and create endpoints

--- src/main/java/ar/com/mediaranking/controller/SeriesController.java
+++ src/main/java/ar/com/mediaranking/controller/SeriesController.java
@@ -1,5 +1,44 @@
 package ar.com.mediaranking.controller;
 
+import ar.com.mediaranking.models.request.SeriesRequest;
+import ar.com.mediaranking.models.response.SeriesResponse;
+import ar.com.mediaranking.service.ISeriesService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/series")
+@Schema(name = "Series Controller", description = "Controller for series endpoints")
 public class SeriesController {
 
+    @Autowired
+    private ISeriesService service;
+
+    @Transactional
+    @Operation(summary = "Creates a new series")
+    public ResponseEntity<SeriesResponse> createSeries(@Valid @RequestBody SeriesRequest request) {
+        SeriesResponse response = new SeriesResponse();
+
+        if (service.isNull(request))
+            return ResponseEntity.badRequest().body(response);
+
+        response = service.save(request);
+
+        return ResponseEntity.created(null).body(response);
+    }
+
+    @Operation(summary = "Get a series by ID")
+    public ResponseEntity<List<SeriesResponse>> getAllSeries() {
+        return ResponseEntity.ok(service.getAll());
+    }
 }
