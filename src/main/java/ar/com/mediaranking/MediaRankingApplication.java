100d78ae1a0afd2c58513864ce6acdcd217cdd6d
gutierrezMartinIvan <martin192012@gmail.com>
2023-01-03 16:06:39 -0300

    added the whole basic implementation to get a serie by its id

--- src/main/java/ar/com/mediaranking/MediaRankingApplication.java
+++ src/main/java/ar/com/mediaranking/MediaRankingApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MediaRankingApplication {
+public class MediaRankingApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MediaRankingApplication.class, args);
