b2f8d8092706218c744ab46055a2edfe5a61729e
Martin Pata <mpata2000@gmail.com>
2023-01-06 15:58:53 -0300

    ADD schema to review

--- src/main/java/ar/com/mediaranking/models/request/ReviewRequest.java
+++ src/main/java/ar/com/mediaranking/models/request/ReviewRequest.java
@@ -1,5 +1,6 @@
 package ar.com.mediaranking.models.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,14 +19,17 @@ public class ReviewRequest {
     @NotNull(message = "User ID can not be null")
     @NotBlank(message = "User ID can not be blank")
     @NotEmpty(message = "User ID can not be empty")
+    @Schema(example = "1")
     private Long userId;
     @NotNull(message = "review can not be null")
     @NotBlank(message = "review can not be blank")
     @NotEmpty(message = "review can not be empty")
+    @Schema(example = "This is a review")
     private String review;
 
     @NotNull(message = "Rating can not be null")
     @NotBlank(message = "Rating can not be blank")
     @NotEmpty(message = "Rating can not be empty")
+    @Schema(description = "Rating must be between 1 and 10", example = "9")
     private Integer rating;
 }
--- src/main/java/ar/com/mediaranking/models/response/ReviewResponse.java
+++ src/main/java/ar/com/mediaranking/models/response/ReviewResponse.java
@@ -1,5 +1,6 @@
 package ar.com.mediaranking.models.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -18,14 +19,17 @@ public class ReviewResponse {
     @NotNull(message = "User ID can not be null")
     @NotBlank(message = "User ID can not be blank")
     @NotEmpty(message = "User ID can not be empty")
+    @Schema(example = "1")
     private String userId;
     @NotNull(message = "review can not be null")
     @NotBlank(message = "review can not be blank")
     @NotEmpty(message = "review can not be empty")
+    @Schema(example = "This is a review")
     private String review;
 
     @NotNull(message = "Rating can not be null")
     @NotBlank(message = "Rating can not be blank")
     @NotEmpty(message = "Rating can not be empty")
+    @Schema(description = "Rating must be between 1 and 10", example = "9")
     private String rating;
 }
