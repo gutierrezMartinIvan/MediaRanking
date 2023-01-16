7a66d00c80a727ba15e2fd91b9113d2a48ffe6cd
Martin Pata <mpata2000@gmail.com>
2023-01-16 13:34:22 -0300

    Add allowable values to genres

--- src/main/java/ar/com/mediaranking/utils/Genres.java
+++ src/main/java/ar/com/mediaranking/utils/Genres.java
@@ -25,13 +25,6 @@ public enum Genres {
     WESTERN;
 
 
-    public static final String[] ALLOWABLE_VALUES = new String[]{
-            "ACTION", "ADVENTURE", "ANIMATION", "BIOGRAPHY", "COMEDY", "CRIME",
-            "DOCUMENTARY", "DRAMA", "FAMILY", "FANTASY", "FILM_NOIR", "HISTORY",
-            "HORROR", "MUSIC", "MUSICAL", "MYSTERY", "ROMANCE", "SCI_FI", "SPORT",
-            "THRILLER", "WAR", "WESTERN"
-    };
-
     public String getDisplayName() {
         return switch (this) {
             case SCI_FI -> "SCI-FI";
