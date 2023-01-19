2a15ae321a36fc0eeb2215cf6bc13b8a5da6a70f
Damian Nuñez Kohler <dnunezko@itba.edu.ar>
2023-01-18 21:28:58 -0300

    added method to find user by email

--- src/main/java/ar/com/mediaranking/models/repository/UserRepository.java
+++ src/main/java/ar/com/mediaranking/models/repository/UserRepository.java
@@ -3,7 +3,9 @@ package ar.com.mediaranking.models.repository;
 import ar.com.mediaranking.models.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
-
+    Optional<UserEntity> findByEmail(String email);
 }
