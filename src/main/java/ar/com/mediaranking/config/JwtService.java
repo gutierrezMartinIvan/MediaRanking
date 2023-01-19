527ac6b808379fe8c36917071831d69a55eabaf4
Damian Nuñez Kohler <dnunezko@itba.edu.ar>
2023-01-19 16:40:45 -0300

    added methods to check if token is valid or expired

--- src/main/java/ar/com/mediaranking/config/JwtService.java
+++ src/main/java/ar/com/mediaranking/config/JwtService.java
@@ -3,6 +3,7 @@ package ar.com.mediaranking.config;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,15 @@ public class JwtService {
 
     private static final String SECRET_KEY = "4B614E645267556B58703273357638792F423F4528482B4D6251655368566D59";
     private static final Integer TOKEN_DURATION_MILLIS = 1000 * 60 * 60 * 24;
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
 
     public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
@@ -34,6 +44,10 @@ public class JwtService {
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
