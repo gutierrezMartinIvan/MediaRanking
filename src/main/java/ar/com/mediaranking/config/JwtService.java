cddd8dba955cc13e1c1db1f6a9496214cd2c88a8
Damian Nuñez Kohler <dnunezko@itba.edu.ar>
2023-01-19 00:27:59 -0300

    created service to process jwt tokens

---
+++ src/main/java/ar/com/mediaranking/config/JwtService.java
@@ -0,0 +1,47 @@
+package ar.com.mediaranking.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "4B614E645267556B58703273357638792F423F4528482B4D6251655368566D59";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        try {
+            Jws<Claims> jws = Jwts.parserBuilder()
+                    .setSigningKey(getSignInKey())
+                    .build()
+                    .parseClaimsJws(token);
+
+            return jws.getBody();
+        } catch(JwtException e) {
+
+        }
+
+        return null;
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
