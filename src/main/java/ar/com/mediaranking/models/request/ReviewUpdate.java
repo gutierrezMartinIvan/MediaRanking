3395375e071dc722807b7befad58a1da370428e7
Damian Nuñez Kohler <dnunezko@itba.edu.ar>
2023-01-24 12:01:24 -0300

    more merge fixes

--- src/main/java/ar/com/mediaranking/models/request/ReviewUpdate.java
+++ src/main/java/ar/com/mediaranking/models/request/ReviewUpdate.java
@@ -3,7 +3,6 @@ package ar.com.mediaranking.models.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
